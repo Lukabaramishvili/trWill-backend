949478fd1432f765bae3aa7c1f48ff380ca9b15a
github username <Lukabaramishvili@gmail.com>
2020-07-20 15:39:13 -0400

    fix cors err

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'https://trawill.netlify.app'
+    origins '*'
 
     resource '*',
       headers: :any,
